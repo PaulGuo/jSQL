d3b0c98ba493cab3b02a4b91200df08ececfe598
hebuliang <hebuliang@gmail.com>
2012-05-16 16:32:20 +0800

    pull

--- jsql.js
+++ jsql.js
@@ -14,15 +14,16 @@
 	}
 	
 	jSQL = function() {
-		this._jSQL = _jSQL;
-		this._DB = _DB;
-		this._currentDB = null;
 		this.init.apply(this,arguments);
 	};
 	
 	jSQL.prototype = {
 		init: function() {
-			
+			this._jSQL = _jSQL;
+			this._DB = _DB;
+			this._currentDB = null;
+			this._buffer = null;
+			this._scopeChain = null;
 		},
 		create: function(dbname, db) {
 			if(this._DB[dbname]) {
@@ -32,39 +33,120 @@
 			this._DB[dbname] = db;
 			return this;
 		},
-		
 		use: function(dbname) {
 			this._currentDB = this._DB[dbname];
 			return this;
 		},
-		
-		
 		/**
-		 * select object from currentDB
-		 * @param key 
-		 		'x': return base value which key is 'x' 
-		 		'*':return all 
-		 		'x.y.z ...': return deep value x->y->x
-		 * 
-		 */
+		* select object from currentDB
+		* @param key
+			'*':	 return all
+			'a':	 return base value which key is 'a'
+			'a.b.c': return deep value a->b->c
+		*
+		*/
 		select: function(key) {
-			// TODO
-		},
-		
-		where: function(){
+			this._scopeChain = null;
 			
+			if(!this._currentDB) {
+				throw('Please Select Database First.');
+			}
+			
+			if(key==='*') {
+				this._buffer = this._currentDB;
+			}
+			
+			this._scopeChain = key;
+			
+			this._buffer = this.iterate(function(data) {
+				return this._deep(data, this._scopeChain);
+			});
+			
+			return this;
 		},
-		
 		count: function() {
 			
 		},
-		
-		orderby: function() {
+		orderby: function(field, order) {
+			var _array = this._objectToArray(this._buffer);
+			var _this = this;
 			
+			if(order && order.toLowerCase() === 'asc') {
+				_array.sort(function(a, b) {
+					return _this._deep(a, field) - _this._deep(b, field);
+				});
+			} else {
+				_array.sort(function(a, b) {
+					return _this._deep(b, field) - _this._deep(a, field);
+				});
+			}
+			
+			_array = this._arrayToObject(_array);
+			this._buffer = _array;
+			return this;
+		},
+		where: function() {
+			this._buffer = _currentDB;
+			return this;
+		},
+		iterate: function(fn) {
+			var _tmp = {};
+			this._buffer = this._buffer || this._currentDB;
+			
+			for(var i in this._buffer) {
+				if(this._buffer.hasOwnProperty(i)) {
+					_tmp[i] = fn.call(this, this._buffer[i]) || this._buffer[i];
+				}
+			}
+			return _tmp;
+		},
+		findAll: function() {
+			return this._buffer;
 		},
 		
-		iterate: function() {
+		_deep: function(data, scope) {
+			var _tmp = data, scope = scope.split('.');
+			for(var i = 0; i < scope.length; i++) {
+				_tmp = _tmp[scope[i]];
+			}
+			return _tmp;
+		},
+		_isArray: function(obj) {
+			return toString.call(obj) === '[object Array]';
+		},
+		_isObject: function(obj) {
+			return obj === Object(obj);
+		},
+		_clone: function(obj) {
+			var _tmp = {};
 			
+			if (!this._isObject(obj)) return obj;
+			if(this._isArray(obj)) return obj.slice();
+			
+			for(var i in obj) {
+				if(obj.hasOwnProperty(i)) {
+					_tmp[i] = obj[i];
+				}
+			}
+			return _tmp;
+		},
+		_objectToArray: function(object) {
+			var array = [], object = this._clone(object);
+			for(var i in object) {
+				if(object.hasOwnProperty(i)) {
+					object[i].__jSQL_Key = i;
+					array.push(object[i]);
+				}
+			}
+			return array;
+		},
+		_arrayToObject: function(array, key) {
+			var object = {};
+			for(var i = 0; i < array.length; i++) {
+				object[array[i][key || '__jSQL_Key']] = array[i];
+				delete object[array[i][key || '__jSQL_Key']][key || '__jSQL_Key'];
+			};
+			return object;
 		}
 	};
 	
