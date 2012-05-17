62893d94843049a7da5645a7cdfb662232e13e34
GuoKai <badkaikai@gmail.com>
2012-05-17 18:43:45 +0800

    replace space with tab.

--- jsql.js
+++ jsql.js
@@ -122,14 +122,14 @@
 		},
 		
 		/**
-        * calculate the count of spec key
-        * @param key
+		* calculate the count of spec key
+		* @param key
 			'*':	 return first package key count
 			'a':	 return base value which key is 'a'
 			'a.b.c': return deep value a->b->c
-        *
-        * @use [jSQL instance].count('a.b.c')
-        */
+		*
+		* @use [jSQL instance].count('a.b.c')
+		*/
 		
 		total: function(key) {
 		    var rs = 0, scope = key.split('.');
