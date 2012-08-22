a24338000ebd5b5d3803d3ff078503f4cffa8145
GuoKai <badkaikai@gmail.com>
2012-08-22 12:36:13 +0800

    update array data source default key_index.

--- jsql.js
+++ jsql.js
@@ -467,7 +467,7 @@
         _appendKey: function(list, indexList) {
             var that = this;
             that._each(list, function(o, i, r) {
-                o[jSQL_KEY_NAME] = that._keygen(o, indexList);
+                o[jSQL_KEY_NAME] = that._keygen(o, indexList) || i;
             });
         },
 
